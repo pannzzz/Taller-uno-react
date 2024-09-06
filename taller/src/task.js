f75f77a72de3c8c7c2e754f445cc4e1c70740799
Panzzz23 <maria.jimenez544@soy.sena.edu.co>
2024-09-05 19:49:58 -0500

    Creacion de task.js
    Se creo task para hacer un props y mostrar algo

---
+++ taller/src/task.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function tarea(props) {
+    return (
+        <div>
+            <h1>Estudiar JavaScript</h1>
+            <h1>El estudiante es: {props.name}</h1>
+        </div>
+    );
+}
+
+export default tarea;
